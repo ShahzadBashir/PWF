802ab575ec47ccbcd4563b19777ac9fddd5d7303
milija.ivovic <milija.ivovic@outlook.com>
2022-01-29 13:50:55 +0100

    Add Footer to Home Screen. Import fontawsome lib and fonts.

--- PWF.Web/Program.cs
+++ PWF.Web/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-builder.Services.AddDbContext<PWFContext>(option => option.UseSqlServer("Server=DESKTOP-T42HDB4\\SQLEXPRESS; Database=PWFDb; Trusted_Connection=True;"));
+builder.Services.AddDbContext<PWFContext>(option => option.UseSqlServer("Server=MILIJAX\\SQLEXPRESS; Database=PWFDb; Trusted_Connection=True;"));
 //builder.Services.AddDbContext<PWFContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection1"))); 
 builder.Services.AddRazorPages();
 
