fdd957e7891955642be3462e2d1d347ac66b1b68
Ramin-Guliyev <ra1000ishere@outlook.com>
2022-01-29 15:13:38 +0400

    Added SendGrid EmailService

---
+++ PWF.Services/Settings/SendGridEmailSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace PWF.Services.Settings;
+
+public class SendGridEmailSettings
+{
+    public string ApiKey { get; set; }
+    public string Mail { get; set; }
+    public string SenderName { get; set; }
+}
